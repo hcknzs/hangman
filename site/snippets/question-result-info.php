fb9dc0d7368c0e4efbd2a994cdf4a513b11ca3fa
Claus Reiser <claus@yeah.de>
2024-07-23 11:16:15 +0200

    style results

--- site/snippets/question-result-info.php
+++ site/snippets/question-result-info.php
@@ -1,6 +1,11 @@
 <article index="<?= $index ?>" class="box" id="<?= $item->id() ?>">
-	<div class="content is-size-7">
-		<h3 class="is-size-6"><?= $item->question() ?></h3>
-		<p><?= str_replace("\n\n", "</p>\n<p>", $item->annotation()) ?></p>
+	<div class="content">
+		<h3 class="is-size-5"><?= $item->question() ?></h3>
+		<?= kirbytext($item->annotation()); ?>
+
+		<h4 class="is-size-6">Quelle</h4>
+		<?= kirbytext($item->sourcetext()) ?>
+
+		<?= kirbytext($item->source()) ?>
 	</div>
 </article>
